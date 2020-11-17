82aa9b644c3a0f0f1ed71ead7e3cd11160f2da93
SMD_ASY <andrii.sydorov@googlemail.com>
2020-11-17 23:05:59 +0300

    was decided new tasks Word a10n (abbreviation)

---
+++ src/tasks/Word_a10n.java
@@ -0,0 +1,72 @@
+package tasks;
+
+import java.util.Scanner;
+
+/**
+ * The word i18n is a common abbreviation of internationalization in the
+ * developer community, used instead of typing the whole word and trying to
+ * spell it correctly. Similarly, a11y is an abbreviation of accessibility.
+ * 
+ * Write a function that takes a string and turns any and all "words" (see
+ * below) within that string of length 4 or greater into an abbreviation,
+ * following these rules:
+ * 
+ * A "word" is a sequence of alphabetical characters. By this definition, any
+ * other character like a space or hyphen (eg. "elephant-ride") will split up a
+ * series of letters into two words (eg. "elephant" and "ride"). The abbreviated
+ * version of the word should have the first letter, then the number of removed
+ * characters, then the last letter (eg. "elephant ride" => "e6t r2e").
+ * 
+ * Example
+ * 
+ * abbreviate("elephant-rides are really fun!") 
+ * //          ^^^^^^^^*^^^^^*^^^*^^^^^^*^^^* 
+ * // words (^): "elephant" "rides" "are" "really" "fun" 
+ * //              123456     123     1     1234     1 
+ * // ignore short words:             X              X
+ * // abbreviate:  "e6t"     "r3s" "are"    "r4y"  "fun" 
+ * // all non-word characters (*) remain in place 
+ * //                     "-"     " "   " "      " "    "!" 
+ * === "e6t-r3s are r4y fun!"
+ * 
+ * @author Zver
+ *
+ */
+public class Word_a10n {
+	public static void main(String[] args) {
+		System.out.println("Enter your word's");
+		Scanner sc = new Scanner(System.in);
+		String in = sc.nextLine();
+		sc.close();
+		System.out.println(abbreviate(in));
+	}
+
+	public static String abbreviate(String string) {
+		String res = "";
+		String temp = "";
+		char[] chArr = string.toCharArray();
+		for (int i = 0; i < chArr.length; i++) {
+			if (Character.isLetter(chArr[i])) {
+				temp += Character.toString(chArr[i]);
+			} else {
+				if (temp.length() <= 3) {
+					res += temp;
+				} else {
+					res += Character.toString(temp.charAt(0)) + (temp.length() - 2)
+							+ Character.toString(temp.charAt(temp.length() - 1));
+				}
+				res += Character.toString(chArr[i]);
+				temp = "";
+			}
+		}
+		if (temp.length() <= 3) {
+			res += temp;
+		} else {
+			res += Character.toString(temp.charAt(0)) + (temp.length() - 2)
+					+ Character.toString(temp.charAt(temp.length() - 1));
+		}
+		return res;
+	}
+}
+
+//internationalization
