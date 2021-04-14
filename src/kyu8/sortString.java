e0215e5977da17791ec8d4b290b935bc34086d39
SMD_ASY <andrii.sydorov@googlemail.com>
2021-04-14 20:44:18 +0300

    Commited with Eclipse

--- src/kyu8/sortString.java
+++ src/kyu8/sortString.java
@@ -5,12 +5,24 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * You will be given a vector of strings. You must sort it alphabetically
+ * (case-sensitive, and based on the ASCII values of the chars) and then return
+ * the first value.
+ * 
+ * The returned value must be a string, and have "***" between each of its
+ * letters.
+ * 
+ * You should not remove or add elements from/to the array.
+ * 
+ */
+
 public class sortString {
 
     public static void main(String[] args) {
-        System.out.println("Enter your's String array:");
         String[] s = {"bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"};
         System.out.println(twoSort(s));
+     //   System.out.println(twoSortWithOutCollections(s));
     }
 
     public static String twoSort(String[] s) {
@@ -23,19 +35,41 @@ public class sortString {
     }
 
     public static String twoSortWithOutCollections(String[] s) {
-        String ans;
+        StringBuilder sb = new StringBuilder();
+        String ans = null;
         int[] stringLength = new int[s.length];
         int maxLength = 0;
         for(int i = 0; i < stringLength.length; i++) {
             stringLength[i] = s[i].length();
             if(maxLength >= s[i].length()) {
                 maxLength = s[i].length();
+                if(maxLength == s[i].length()) {
+                    sb.append(s[i]).append(" ");
+                } else {
+                    sb.setLength(0);
+                }
             }
         }
-        
-        for(int i = 0; i < stringLength.length; i++) {
-
+        String[] stringWithMaxLength = null;
+        if(sb.toString().trim().contains(" ")) {
+            stringWithMaxLength = sb.toString().split(" ");
+        } else {
+            stringWithMaxLength = new String[1];
+            stringWithMaxLength[0] = sb.toString();
         }
+
+        
+        for(int j = 0; j < stringWithMaxLength[0].length(); j++) {
+            char[] letter = new char[stringWithMaxLength.length];
+            char max = stringWithMaxLength[0].charAt(j);
+            for(int i = 0; i < stringWithMaxLength.length; i++) {
+                letter[j] = stringWithMaxLength[i].charAt(j);
+                if(max <= letter[j]) {
+                    max = letter[j];
+                }
+            } 
+        }
+        
         return ans;
     }
 }
