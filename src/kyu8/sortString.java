9e56d6e670aeb8eed2d6ac56ef941d378cf7e3a1
SMD_ASY <andrii.sydorov@googlemail.com>
2021-04-14 17:15:12 +0300

    another Solution of sortingtasks

--- src/kyu8/sortString.java
+++ src/kyu8/sortString.java
@@ -1,6 +1,5 @@
 package kyu8;
 
-import java.util.Scanner;
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
@@ -10,9 +9,7 @@ public class sortString {
 
     public static void main(String[] args) {
         System.out.println("Enter your's String array:");
-        Scanner sc = new Scanner(System.in);
-        String[] s = sc.nextLine().split(" ");
-        sc.close();
+        String[] s = {"bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"};
         System.out.println(twoSort(s));
     }
 
@@ -21,13 +18,23 @@ public class sortString {
         Collections.sort(ls);
         String temp = ls.get(0);
         char[] ch = temp.toCharArray();
-        int numberStart = 3;
-        String ans = "";
-        for(int i = 0; i < ch.length; i++) {
-            ans += Character.toString(ch[i]);
-            for(int j = 0; j < numberStart; j++) {
-                ans += "*";
+        String ans = String.join("***", ls.get(0).split(""));
+        return ans;
+    }
+
+    public static String twoSortWithOutCollections(String[] s) {
+        String ans;
+        int[] stringLength = new int[s.length];
+        int maxLength = 0;
+        for(int i = 0; i < stringLength.length; i++) {
+            stringLength[i] = s[i].length();
+            if(maxLength >= s[i].length()) {
+                maxLength = s[i].length();
             }
+        }
+        
+        for(int i = 0; i < stringLength.length; i++) {
+
         }
         return ans;
     }
