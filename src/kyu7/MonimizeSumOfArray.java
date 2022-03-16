53b22777e521b1f61868615a8b7aa90ba1f473eb
SMD_ASY <andrii.sydorov@googlemail.com>
2022-03-16 22:20:45 +0200

    The kata of product array was done

--- src/kyu7/MonimizeSumOfArray.java
+++ src/kyu7/MonimizeSumOfArray.java
@@ -34,8 +34,12 @@ public class MonimizeSumOfArray {
         // int n = Integer.valueOf(sc.nextLine());
         // System.out.println(nthSmallest(arr, n));
 
-        int[] arr = Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::valueOf).toArray();
-        System.out.println(maxGap(arr));
+        // int[] arr =
+        // Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::valueOf).toArray();
+        // System.out.println(maxGap(arr));
+
+        int[] numbers = Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::valueOf).toArray();
+        System.out.println(Arrays.toString(productArray(numbers)));
         sc.close();
     }
 
@@ -348,6 +352,88 @@ public class MonimizeSumOfArray {
             dif = Math.max(dif, numbers[i] - numbers[i - 1]);
         }
         return dif;
+    }
+
+    /**
+     * Task
+     * Given an array/list [] of integers , Construct a product array Of same size
+     * Such That prod[i] is equal to The Product of all the elements of Arr[] except
+     * Arr[i].
+     * 
+     * Notes
+     * Array/list size is at least 2 .
+     * 
+     * Array/list's numbers Will be only Positives
+     * 
+     * Repetition of numbers in the array/list could occur.
+     * 
+     * Input >> Output Examples
+     * productArray ({12,20}) ==> return {20,12}
+     * Explanation:
+     * The first element in prod [] array 12 is the product of all array's elements
+     * except the first element
+     * 
+     * The second element 20 is the product of all array's elements except the
+     * second element .
+     * 
+     * productArray ({1,5,2}) ==> return {10,2,5}
+     * Explanation:
+     * The first element 10 is the product of all array's elements except the first
+     * element 1
+     * 
+     * The second element 2 is the product of all array's elements except the second
+     * element 5
+     * 
+     * The Third element 5 is the product of all array's elements except the Third
+     * element 2.
+     * 
+     * productArray ({10,3,5,6,2}) return ==> {180,600,360,300,900}
+     * Explanation:
+     * The first element 180 is the product of all array's elements except the first
+     * element 10
+     * 
+     * The second element 600 is the product of all array's elements except the
+     * second element 3
+     * 
+     * The Third element 360 is the product of all array's elements except the third
+     * element 5
+     * 
+     * The Fourth element 300 is the product of all array's elements except the
+     * fourth element 6
+     * 
+     * Finally ,The Fifth element 900 is the product of all array's elements except
+     * the fifth element 2
+     * 
+     * @param numbers
+     * @return a product array Of same size
+     *         Such That prod[i] is equal to The Product of all the elements of
+     *         Arr[] except
+     *         Arr[i].
+     * 
+     */
+    public static long[] productArray(int[] numbers) {
+        long[] ans = new long[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            ans[i] = calculateProd(numbers, i);
+        }
+        return ans;
+    }
+
+    /**
+     * 
+     * @param array of int numbers
+     * @param int i
+     * @return the product of the elements of array, except the element with index i
+     */
+    public static long calculateProd(int[] numbers, int i) {
+        long prod = 1;
+        for (int j = 0; j < numbers.length; j++) {
+            if (i == j) {
+                continue;
+            }
+            prod *= numbers[j];
+        }
+        return prod;
     }
 
 }
