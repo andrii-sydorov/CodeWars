392b8e984de4a51ae5fcd5486adffe0e8d0e5421
SMD_ASY <andrii.sydorov@googlemail.com>
2022-03-16 23:23:40 +0200

    The kata find the minimum value from digits of array without replications was done

--- src/kyu7/MonimizeSumOfArray.java
+++ src/kyu7/MonimizeSumOfArray.java
@@ -48,8 +48,16 @@ public class MonimizeSumOfArray {
         // Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::valueOf).toArray();
         // System.out.println(minimumNumber(numbers));
 
-        int[] numbers = Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::valueOf).toArray();
-        System.out.println(maxTriSum(numbers));
+        // int[] numbers =
+        // Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::valueOf).toArray();
+        // System.out.println(maxTriSum(numbers));
+
+        // int[] weights =
+        // Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::valueOf).toArray();
+        // System.out.println(rowWeights(weights));
+
+        int[] values = Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::valueOf).toArray();
+        System.out.println(minValue(values));
         sc.close();
     }
 
@@ -687,5 +695,45 @@ public class MonimizeSumOfArray {
             }
         }
         return ans;
+    }
+
+    /**
+     * Task
+     * Given a list of digits, return the smallest number that could be formed from
+     * these digits, using the digits only once (ignore duplicates).
+     * 
+     * Notes:
+     * Only positive integers will be passed to the function (> 0 ), no negatives or
+     * zeros.
+     * Input >> Output Examples
+     * minValue ({1, 3, 1}) ==> return (13)
+     * Explanation:
+     * (13) is the minimum number could be formed from {1, 3, 1} , Without
+     * duplications
+     * 
+     * minValue({5, 7, 5, 9, 7}) ==> return (579)
+     * Explanation:
+     * (579) is the minimum number could be formed from {5, 7, 5, 9, 7} , Without
+     * duplications
+     * 
+     * minValue({1, 9, 3, 1, 7, 4, 6, 6, 7}) return ==> (134679)
+     * Explanation:
+     * (134679) is the minimum number could be formed from {1, 9, 3, 1, 7, 4, 6, 6,
+     * 7} , Without duplications
+     * 
+     * @param values - an integer array
+     * @return the smallest number that could be formed from
+     *         these digits, using the digits only once (ignore duplicates)
+     */
+    public static int minValue(int[] values) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < values.length; i++) {
+            set.add(String.valueOf(values[i]));
+        }
+        String temp = "";
+        for (String s : set) {
+            temp += s;
+        }
+        return Integer.valueOf(temp);
     }
 }
