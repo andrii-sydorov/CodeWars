9d553b0a8d67b3494d3d1500dfd1f50aae91d0b8
SMD_ASY <andrii.sydorov@googlemail.com>
2022-10-22 14:50:30 +0300

    The Tribonacci kata was done. The tests were implemented.

--- src/kyu6/TestCase.java
+++ src/kyu6/TestCase.java
@@ -72,4 +72,15 @@ class TestCase {
                 () -> assertEquals("camelcasingtest", BreakCamelCase.camelCase2("camelcasingtest")));
     }
 
+    @Test
+    void testXbonaaci() {
+        double precision = 1e-10;
+        assertArrayEquals(new double[] { 1, 1, 1, 3, 5, 9, 17, 31, 57, 105 },
+                Xbonacci.tribonacci(new double[] { 1, 1, 1 }, 10), precision);
+        assertArrayEquals(new double[] { 0, 0, 1, 1, 2, 4, 7, 13, 24, 44 },
+                Xbonacci.tribonacci(new double[] { 0, 0, 1 }, 10), precision);
+        assertArrayEquals(new double[] { 0, 1, 1, 2, 4, 7, 13, 24, 44, 81 },
+                Xbonacci.tribonacci(new double[] { 0, 1, 1 }, 10), precision);
+    }
+
 }
